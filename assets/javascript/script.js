1de9850566b5cf0e3500a205dfe4fa6b7cd577e9
mtking78 <mtking78@gmail.com>
2018-03-06 11:04:22 -0700

    Basic html complete.  Firebase connection active.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,7 +3,7 @@
 // After the players make their selection, the game will tell them whether a tie occurred or if one player defeated the other.
 // The game will track each player's wins and losses.
 // Throw some chat functionality in there!
-document.onload(alert("hi!"));
+//document.onload(alert("hi!"));
 
 // Initialize Firebase
 var config = {
@@ -17,9 +17,48 @@ var config = {
   firebase.initializeApp(config);
 // Assign the reference to the database to a variable named 'database'
 var database = firebase.database();
+var p1 = [
+  Name = "",
+  Wins = 0,
+  Losses = 0,
+  Ties = 0
+];
+
+var p2 = [
+  Name = "",
+  Wins = 0,
+  Losses = 0,
+  Ties = 0
+];
+
+// Click Button changes what is stored in firebase
+$("#start").on("click", function() {
+  // Prevent the page from refreshing
+  event.preventDefault();
+  // Get inputs
+  var p1Name = $("#userName").val().trim();
+  var p2Name = $("#p2Name").val().trim();
+  // Change what is saved in firebase
+  database.ref().set({
+    p1Name: p1Name,
+    p2Name: p2Name
+  });
+});
+
 // Initial Values
 
 
 // At the initial load and subsequent value changes, get a snapshot of the stored data.
 // This function allows you to update your page in real-time when the firebase database changes.
 //database.ref().on("value", function(snapshot) {});
+
+// name input, start button in html
+// #1Name #start
+// r/p/s buttons
+// #rock #paper #scissors
+// w/l/t counts
+// #p1Wins #p1Losses #p1Ties
+// #p2Wins #p2Losses #p2Ties
+// message area (prepend with user name & text)
+// message box and send button
+// #message #chatArea
